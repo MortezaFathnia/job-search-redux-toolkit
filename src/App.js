2e92d630901d112c469a1bc59388158507961bdb
MortezaFathnia <fathniamorteza@gmail.com>
2023-01-07 14:21:11 +0330

    dashboard init

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-import { Landing, Error, Dashboard, Register } from "./pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Landing, Error, Register } from "./pages";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from "react-toastify";
+import { Profile, AddJob, AllJobs, States, SharedLayout } from './pages/dashboard'
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Dashboard/>}/>
-      <Route path="landing" element={<Landing/>}/>
-      <Route path="register" element={<Register/>}/>
-      <Route path="*" element={<Error/>}/>
-    </Routes>
-    <ToastContainer position="top-center"/>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<States />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+        <Route path="landing" element={<Landing />} />
+        <Route path="register" element={<Register />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <ToastContainer position="top-center" />
     </BrowserRouter>
   );
 }
---
+++ src/pages/dashboard/States.js
@@ -0,0 +1,8 @@
+
+const States = () => {
+  return (
+    <div>state</div>
+  )
+}
+
+export default States
